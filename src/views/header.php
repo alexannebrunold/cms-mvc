192d8638d8176bb70a72ca7b7cbdbc21a6fced82
MaevaWolff <maeva.wolff@hetic.net>
2021-12-04 19:22:45 +0100

    fix error in UserEntity

---
+++ src/views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tp HP</title>
+</head>
+
+<nav>
+    <a class="nav-link" href="/signup">Sign up</a>
+    <a href="/login">Log in</a>
+    
+    <?php if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin'] === "true") : ?>
+         <li>
+            <a href="/">User list</a>
+         </li>
+    <?php endif ?>
+</nav>
